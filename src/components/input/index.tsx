6a18ac09e5aaddc850708b92894cb297e9d984bc
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-04-11 20:56:54 -0300

    Isolando componentes

---
+++ src/components/input/index.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { InputHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons';
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
+  <Container>
+    {Icon && <Icon size={20} />}
+    <input {...rest} />
+  </Container>
+);
+
+export default Input;
